f285b29ac5918aa4c09ab311bf45b5e77d627c4e
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-12 18:25:34 +0530

    Add Hello World program and naming conventions examples

---
+++ main.py
@@ -0,0 +1,11 @@
+#Hello World program in Python
+print("Hello World")
+
+#naming conventions
+PrahmeshPatil="Intern at Avigna AI"#pascal case     
+prathmeshPatil="Intern at Avigna AI "#camel case    
+prathmesh_patil="Intern at Avigna AI"#snake case    
+
+
+
+
