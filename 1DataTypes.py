9a5bb540126d7c4da35c3128ebe04f795f5ccfee
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-13 15:43:00 +0530

    Add 1DataTypes.py with examples of numeric, boolean, and string variables, including type checks and string operations

---
+++ 1DataTypes.py
@@ -0,0 +1,40 @@
+# variables 
+#Numberic Variables 
+i=5#integer 
+j=-23
+k=3.24#float
+l='Rahul'#string    
+m=23j#complex number 
+
+print(type(i))
+print(type(j))  
+print(type(k))
+print(type(l))
+print(type(m))
+
+
+#Boolean Variables
+ab=True
+bc=False  
+
+
+#String
+
+name ="learn with anviga ai 1738@3248)+_(#)"
+print(type(name))
+
+#In string Charactor to Unicode
+a="A"
+print(ord(a))
+#In string Unicode to Charector 
+b=80
+print(chr(b))
+
+#Indexing and Slicing in String 
+c="Prarhmeah patil"
+print(c[4])#indexing
+
+d="avigna Ai"
+print(d[0:6:1])#slicing
+print(d[8:10:1])#slicing   
+
