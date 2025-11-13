e750a5d2d58294893799265054a78e4e4b45849f
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-13 18:53:20 +0530

    Add 5ConditionalStatment.py with examples of if, if-else, and if-elif-else statements

---
+++ 5ConditionalStatment.py
@@ -0,0 +1,25 @@
+# if statment 
+a=23
+if a>19:
+    print("I will do task A")
+ 
+
+# if-else statment        
+
+money=int(input("Enter your Money:"))
+if money<=10:
+    print("you can by toffee")
+else:
+    print("you can by ice-cream")    
+    
+# if-elif-else statment 
+
+money=int(input("Enter Your Money:"))
+if money==10:
+    print("you can by choclate")
+elif money==20:
+    print("you can by ice-cream with choclate")
+else:
+    print("you can by burger")      
+
+
