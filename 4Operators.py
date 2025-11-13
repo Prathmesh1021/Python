b0b032a86e2f501abfd58249e32977eb3fa2602a
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-13 15:44:19 +0530

    Add 4Operators.py with examples of arithmetic, assignment, comparison, logical operators, and trivial questions

---
+++ 4Operators.py
@@ -0,0 +1,56 @@
+#Arithmatic Operators 
+a=10
+b=5
+print("Addition:",a+b)
+print("Substraction:",a-b)
+print("multipication:",a*b)
+print("Division:",a/b)
+print("flooor Division:",a//b)
+print("modulus:",a%b)
+print("Exponention:",a**b)
+print("************************************")
+#Assignment Operators
+c=24
+print(c)
+print("************************************")
+#compound Assignment Operators
+d=10
+d+=20
+d+=45
+'''d-=
+d*=
+d/=    
+d//
+d**=  
+'''
+print(d)
+print("************************************")
+#Comparison Operators
+e=12.1
+b=12
+print(e == b)
+print(e != b)
+print(e >b)
+print( e > b)
+print(23 > 23)
+print(23 >= 23)
+print(45<=45)
+
+print(ord("A"))
+print(ord("B"))
+
+print("A">"B")
+print("A"<"B")  
+print("*************************************") 
+#Logical operators
+
+print(10 > 11 and 34 == 34 and 34 > 4 and 45 < 90)
+print(10 < 5 or 34 != 34 or 224>35 or 33>435 or 50>34)
+print(not(10 < 5))  
+print("*************************************") 
+
+#Trivial Questions
+print(126>130)
+print( 456==456)!=(235==236)
+print(12 < 10 or 45 == 56 or 15!=13)
+print(True and bool(0))
